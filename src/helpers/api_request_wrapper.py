88a7d7f84cd195896f6337d1fd79b056037479d6
vasanthpannir <bmsmnti.123@gmail.com>
2024-04-12 18:50:42 +0530

    12-04-2024

---
+++ src/helpers/api_request_wrapper.py
@@ -0,0 +1,12 @@
+import requests
+from src.constants.api_constants import Base_url,base_url,APIconstants
+
+print(Base_url)
+
+func_url = base_url()
+print(func_url)
+
+class_url = APIconstants.base_url()
+print(class_url)
+
+
